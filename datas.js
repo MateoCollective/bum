87afcd7cce2eaf8bf2e052a17f7e72d6d12f8d99
Egnoo CTE <84653206+MateoCollective@users.noreply.github.com>
2023-06-23 02:43:30 +0700

    Add files via upload

--- datas.js
+++ datas.js
@@ -46,121 +46,65 @@ function showModal(siswa) {
 
 
 
-  <body>
   <div class="resume">
-      <div class="profile-pic">
-      <img src="${siswa.photo_profile}" alt="${siswa.nama}">
-  </div>
-  <h1>${siswa.nama}</h1>
-    <hr>
-  <div class="section">
-      <h2">KATA-KATA</h2>
-      <p> To impart my knowledge and serving skills earned through extensive trainings and experiences and further develop myself in being a part of the company.</p>
-    </div>
-  <hr>
-  <div class="section">
-    <h2">PERSONAL INFORMATION</h2>
-      <p><strong>Nama Panggilan:</strong> ${siswa.nama_panggilan}</p>
-      <p><strong>Tanggal Lahir:</strong> ${siswa.tanggal_lahir}</p>
-      <p><strong>Tempat Lahir:</strong> ${siswa.tempat_lahir}</p>
-      <p><strong>Alamat Email:</strong> ${siswa.alamat_email}</p>
-      <p><strong>Place Of Birth:</strong> </p>
-      <p><strong>Civil Status:</strong> Single</p> 
-      <p><strong>Religion:</strong> Roman Catholic</p>
-      <p><strong>Nationality:</strong> Filipino</p>
-    </div>
-  <hr>
-  <div class="section">
-    <h2">CONTACT</h2>
-      <p><strong>Cellphone Number:</strong> 09102949352</p>
-      <p><strong>Email Address:</strong> afable.nicole.eccinfoodtech@gamail.com</p>
-   </div>
-  <hr>
-  <div class="section">
-  <h2>EDUCATION</h2>
-      <p><strong>COLLEGE:</strong>
-      <p>- Bachelor of Science and Industrial Technology Major in FoodTech (2nd year College) 2021-2023 |EARIST Eulogio "Amang" Rodriguez Institute of Science and Technology </p>
-      <p><strong>SECONDARY:</strong>
-      <p>- Information and Communication Technology | Philippine Technological Institute of Science Arts and Trade Inc. (Senior High). <p> -
-  GMA, Cavite 2018-2020 Gen. Mariano Alvarez Technical High School (Junior High) #688 Brgy. Poblacion 1 Cong. Road GMA, Cavite 2012-2018</p>
-  
-      <p><strong>PRIMARY:</strong>
-      <p>- San. Gabriel 1. Elementary School Barangay Dacon, GMA, Cavite Year Graduated - 2014 </p>
-  </div>
-  <hr>
-  <div class="section">
-    <h2">WORK EXPERIENCE</h2>
-      <p><strong>Company Name:</strong> Celeraise Electronics Corporation </p>
-      <p><strong>Date:</strong> September 8, 2020-May 16, 2021
-      <p><strong>Company Location:</strong> Maguyam Silang, Cavite Production Machine Operator</p>
-  
-      </div>
-    </body>
-  </html>
-  
-
-
-
-
-
-
-  
-  <div class="biodata-container">
-  <h1>Biodata Siswa</h1>
   <div class="profile-pic">
     <img src="${siswa.photo_profile}" alt="${siswa.nama}">
   </div>
-  <div class="info">
-    <label>Nama:</label>
-    <p>${siswa.nama}</p>
+ 
+
+  <div class="section-kta">
+      <p> To impart my knowledge and serving skills earned through extensive trainings and experiences and further
+        develop myself in being a part of the company.</p>
   </div>
-  <div class="info">
-    <label>Nama Panggilan:</label>
-    <p>${siswa.nama_panggilan}</p>
-  </div>
-  <div class="info">
-    <label>Tanggal Lahir:</label>
-    <p>${siswa.tanggal_lahir}</p>
-  </div>
-  <div class="info">
-    <label>Tempat Lahir:</label>
-    <p>${siswa.tempat_lahir}</p>
-  </div>
-  <div class="info">
-    <label>Alamat Saat Ini:</label>
-    <p>${siswa.alamat_saat_ini}</p>
-  </div>
-  <div class="info">
-    <label>Alamat Email:</label>
-    <p>${siswa.alamat_email}</p>
-  </div>
-  <div class="info">
-    <label>Nomor Telepon:</label>
-    <p>${siswa.nomor_telepon}</p>
-  </div>
-  <div class="info">
-    <label>Riwayat Pendidikan:</label>
-    <p>
-      ${renderPendidikan(siswa.riwayat_pendidikan)}
-    </p>
-  </div>
-  <div class="info">
-    <label>Prestasi Akademik:</label>
-    <p>
-      ${renderList(siswa.prestasi_akademik)}
-    </p>
-  </div>
-  <div class="info">
-    <label>Aktivitas Ekstrakurikuler:</label>
-    <p>
-      ${renderList(siswa.aktivitas_ekstrakurikuler)}
-    </p>
-  </div>
-  <div class="info">
-    <label>Rencana Masa Depan:</label>
+  <hr>
+
+  <div class="section">
+    <p><strong>Nama Panggilan:</strong> ${siswa.nama_panggilan}</p>
+
+    
+    <h2>Informasi Siswa</h2>
+    <p><strong>Nama:</strong> ${siswa.nama}</p>
+    <p><strong>Tanggal Lahir:</strong> ${siswa.tanggal_lahir}</p>
+    <p><strong>Tempat Lahir:</strong> ${siswa.tempat_lahir}</p>
+    <p><strong>Alamat Saat Ini:</strong> ${siswa.alamat_saat_ini}</p>
+    <p><strong>Alamat Email:</strong> ${siswa.alamat_email}</p>
+    <hr>
+    <h2>Riwayat Pendidikan</h2>
+
+        <p><strong>Nama Sekolah:</strong> ${siswa.riwayat_pendidikan[0].nama_sekolah}</p>
+        <p><strong>Tingkat Pendidikan:</strong> ${siswa.riwayat_pendidikan[0].tingkat_pendidikan}</p>
+        <p><strong>Tahun Kelulusan:</strong> ${siswa.riwayat_pendidikan[0].tahun_kelulusan}</p>
+
+        <p><strong>Nama Sekolah:</strong> ${siswa.riwayat_pendidikan[1].nama_sekolah}</p>
+        <p><strong>Tingkat Pendidikan:</strong> ${siswa.riwayat_pendidikan[1].tingkat_pendidikan}</p>
+        <p><strong>Tahun Kelulusan:</strong> ${siswa.riwayat_pendidikan[1].tahun_kelulusan}</p>
+        <hr>
+    <h2>Prestasi Akademik</h2>
+
+      <p>${siswa.prestasi_akademik[0]}</p>
+      <p>${siswa.prestasi_akademik[1]}</p>
+
+
+    <h2>Aktivitas Ekstrakurikuler</h2>
+  
+      <p>${siswa.aktivitas_ekstrakurikuler[0]}</p>
+      <p>${siswa.aktivitas_ekstrakurikuler[1]}</p>
+
+    <h2>Rencana Masa Depan</h2>
     <p>${siswa.rencana_masa_depan}</p>
+
   </div>
-</div>
+  <hr>
+
+  
+  
+
+
+
+
+
+
+ 
 
   `;
   const modal = document.getElementById('myModal');
